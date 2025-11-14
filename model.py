49debd87256ff0c0ffb3ba16cd70cff18e36689b
BOUKRANI Mohamed <mohamedboukrani7@gmail.com>
2025-11-14 16:13:21 +0100

    commit

--- model.py
+++ model.py
@@ -70,57 +70,78 @@ def train_eval_model(model, train_dataloader, val_dataloader, num_epochs, optimi
     # model = deepcopy(model)
     train_losses = []
     val_losses = []
+    train_accs = []
+    val_accs = []
 
     training_start_time = time.time()
 
     for epoch in range(num_epochs):
-        train_loss = []
-        val_loss = []
+        train_loss = []        
+        train_acc = 0
 
         # === Training phase ===
         model.train()
 
         if verbose:
             print(f'----------------------------------\nEpoch [{epoch+1}/{num_epochs}]\n----------------------------------')    
+        else :
+            print(f'Epoch {epoch+1} ...')
 
         # Iterate over training batches
         for batch, (X, y) in enumerate(train_dataloader):
+            correct_batch_pred = 0
             X = X.to(device)
             y = y.to(device)
 
             # Forward pass
-            optimizer.zero_grad()
             train_pred = model(X)
             loss = loss_fn(train_pred, y)
-            train_loss.append(loss.item())
+
+            train_loss.append(loss.item())            
+            _, pred_labels = torch.max(train_pred, 1)
+            correct_batch_pred = (pred_labels == y).sum().item()
+            train_acc += correct_batch_pred
 
             # Backward pass and parameter update
             loss.backward()
             optimizer.step()
+            optimizer.zero_grad()
 
             # Periodic training feedback
             if verbose:
                 if batch % 200 == 0:
-                    print(f'train loss : {loss.item()} [{batch}/{len(train_dataloader)}]')
+                    print(f'train loss : {loss.item():.2f}, train acc : {correct_batch_pred/len(X):.2f} % [{batch}/{len(train_dataloader)}]')
 
         train_losses.append(np.mean(train_loss))
 
         # === Validation phase ===
-        model.eval()
+        model.eval()        
+        val_loss = []
+        val_acc = 0
+
         with torch.no_grad():
             for X, y in val_dataloader:
+                correct_batch_pred = 0
                 X, y = X.to(device), y.to(device)
+
                 val_pred = model(X)
                 loss = loss_fn(val_pred, y)
-                val_loss.append(loss.item())
 
+                val_loss.append(loss.item())
+                _, pred_labels = torch.max(val_pred, 1)
+                correct_batch_pred = (pred_labels == y).sum().item()
+                val_acc += correct_batch_pred
         val_losses.append(np.mean(val_loss))
 
         if verbose:
-            print(f'\nAvg Train Loss: {np.mean(val_loss):.4f} | Avg Val Loss: {np.mean(train_loss):.4f}')
+            print(f'\nAvg Train Loss: {np.mean(train_losses):.2f}, Train Acc : {(train_acc/len(train_dataloader.dataset))*100:.2f} % | Avg Val Loss: {np.mean(val_losses):.2f} , Val Acc : {(val_acc/len(val_dataloader.dataset))*100:.2f} % ')
+
+        # === Performance storage ===            
+        train_accs.append(train_acc/len(train_dataloader.dataset))        
+        val_accs.append(val_acc/len(val_dataloader.dataset))
 
     # === Summary ===
     duration = time.time() - training_start_time
     print(f'Total training time : {duration // 60:.0f} minutes and {duration % 60:.2f} seconds')
 
-    return model, train_losses, val_losses
+    return model, train_losses, val_losses, train_accs, val_accs
